a94b16cc614b1f38465ee6163df66296abf1dcc9
dai <chamestica@gmail.com>
2019-05-21 04:28:49 +0900

    Reduce using variable in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,8 +9,6 @@ require('laravel-mix-eslint')
 require('laravel-mix-stylelint')
 require('laravel-mix-imagemin')
 
-const svgDummyModuleName = 'assets/js/.svg-dummy-module'
-
 // 1. Replace following 'input-theme-name' to your theme name
 // 2. Rename following directories to your theme name
 //   resources/themes/input-theme-name
@@ -50,7 +48,7 @@ mix
             // In development, keep chunk file without deletion
             // Because error occurs if chunk file has deleted when creating mix-manifest.json
             chunk: {
-              name: svgDummyModuleName,
+              name: 'assets/js/.svg-dummy-module',
               keep: true
             },
             svgo: {
@@ -74,21 +72,45 @@ mix
     `${wpContentThemeDirName}/assets/svg`,
     { base: `${resourcesThemeDirName}/assets/svg` }
   )
+  .browserSync({
+    open: false,
+    host: process.env.BROWSER_SYNC_HOST || 'localhost',
+    port: process.env.BROWSER_SYNC_PORT || 3000,
+    proxy: process.env.BROWSER_SYNC_PROXY || false,
+    // If this setting is `${wpContentThemeDirName}/**/*`,
+    // injection of changes such as CSS will be not available
+    // https://github.com/JeffreyWay/laravel-mix/issues/1053
+    files: [
+      `${wpContentThemeDirName}/assets/**/*`,
+      `${wpContentThemeDirName}/**/*.php`
+    ],
+    https:
+      process.env.BROWSER_SYNC_HTTPS_CERT &&
+      process.env.BROWSER_SYNC_HTTPS_KEY
+        ? {
+          cert: process.env.BROWSER_SYNC_HTTPS_CERT,
+          key: process.env.BROWSER_SYNC_HTTPS_KEY
+        }
+        : false
+    // Reloading is necessary to see the change of the SVG file
+    // But BrowserSync execute ingection for SVG changes
+    // Options of BrowserSync can not change this behavior
+    // https://github.com/BrowserSync/browser-sync/issues/1287
+  })
 
 // Only in production mode
 if (process.env.NODE_ENV === "production") {
-  const patterns = [ 'assets/images/**/*' ]
   mix
     .version()
     // Copy and minify images in production
     .imagemin(
       // Options for copying
-      patterns,
+      [ 'assets/images/**/*' ],
       { context: resourcesThemeDirName },
       // Options for optimization
       {
         // To find targets exactly, requires test option that is function
-        test: filePath => !!multimatch(filePath, patterns).length,
+        test: filePath => !!multimatch(filePath, [ 'assets/images/**/*' ]).length,
         optipng: { optimizationLevel: 0 }, // 0 ~ 7
         gifsicle: { optimizationLevel: 1 }, // 1 ~ 3
         plugins: [ require('imagemin-mozjpeg')({ quality: 100 }) ] // 0 ~ 100
@@ -96,6 +118,7 @@ if (process.env.NODE_ENV === "production") {
     )
     // Delete chunk file for SVG sprite
     .then(() => {
+      const svgDummyModuleName = 'assets/js/.svg-dummy-module'
       fs.removeSync(`${wpContentThemeDirName}/${svgDummyModuleName}.js`)
       const pathToManifest = `${wpContentThemeDirName}/mix-manifest.json`
       const manifest = require(`./${pathToManifest}`)
@@ -106,35 +129,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Only in development mode
 else {
-  // Copy images without minifying in development
-  mix.copyWatched(
-    `${resourcesThemeDirName}/assets/images`,
-    `${wpContentThemeDirName}/assets/images`,
-    { base: `${resourcesThemeDirName}/assets/images` }
-  )
-  if (process.env.BROWSER_SYNC_PROXY) {
-    // Reloading is necessary to see the change of the SVG file
-    // But BrowserSync execute ingection for SVG changes
-    // Options of BrowserSync can not change this behavior
-    // https://github.com/BrowserSync/browser-sync/issues/1287
-    const options = {
-      open: false,
-      host: process.env.BROWSER_SYNC_HOST || 'localhost',
-      port: process.env.BROWSER_SYNC_PORT || 3000,
-      proxy: process.env.BROWSER_SYNC_PROXY || '',
-      // If this setting is `${wpContentThemeDirName}/**/*`,
-      // injection of changes such as CSS will be not available
-      // https://github.com/JeffreyWay/laravel-mix/issues/1053
-      files: [
-        `${wpContentThemeDirName}/assets/**/*`,
-        `${wpContentThemeDirName}/**/*.php`
-      ]
-    }
-    const cert = process.env.BROWSER_SYNC_HTTPS_CERT
-    const key = process.env.BROWSER_SYNC_HTTPS_KEY
-    if (cert && key) {
-      options.https = { cert, key }
-    }
-    mix.browserSync(options)
-  }
+  mix
+    // Copy images without minifying in development
+    .copyWatched(
+      `${resourcesThemeDirName}/assets/images`,
+      `${wpContentThemeDirName}/assets/images`,
+      { base: `${resourcesThemeDirName}/assets/images` }
+    )
 }
